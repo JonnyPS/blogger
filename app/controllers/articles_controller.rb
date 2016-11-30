0c7e0fbffd7feab87f06d82173c747b8f5928cc4
Jonny Spry <aml-jonny@AML-MAC013.local>
2016-11-30 11:52:14 +0000

    Add 'edit article' functionality

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -17,9 +17,16 @@ class ArticlesController < ApplicationController
 		redirect_to article_path(@article)
 	end
 	def destroy
-
 		@articleBin = Article.find(params[:id])
 		@articleBin.destroy
 		redirect_to articles_path
+	end
+	def edit
+		@article = Article.find(params[:id])
+	end
+	def update
+		@article = Article.find(params[:id])
+		@article.update(article_params)
+		redirect_to article_path(@article)
 	end
 end
