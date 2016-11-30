b58477d6ebfac434ab36176d891566b9c88a40db
Jonny Spry <aml-jonny@AML-MAC013.local>
2016-11-30 14:12:21 +0000

    Add a flash notice to announce the creation, editing or deletion of an article

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -14,11 +14,13 @@ class ArticlesController < ApplicationController
 	def create
 		@article = Article.new(article_params)
 		@article.save
+		flash.notice = "Article '#{@article.title}' has been created!"
 		redirect_to article_path(@article)
 	end
 	def destroy
-		@articleBin = Article.find(params[:id])
-		@articleBin.destroy
+		@article = Article.find(params[:id])
+		flash.notice = "Article '#{@article.title}' has been deleted!"
+		@article.destroy
 		redirect_to articles_path
 	end
 	def edit
@@ -27,6 +29,7 @@ class ArticlesController < ApplicationController
 	def update
 		@article = Article.find(params[:id])
 		@article.update(article_params)
+		flash.notice = "Article '#{@article.title}' Updated!"
 		redirect_to article_path(@article)
 	end
 end
