19e4898e582f0d8813aae49e18a7687e3016a37a
Jonny Spry <aml-jonny@AML-MAC013.local>
2017-01-12 17:14:01 +0000

    add gulp to the project, including the task gulp watch

---
+++ gulpfile.js
@@ -0,0 +1,18 @@
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+
+
+// gulp.task('hello', function() {
+//   console.log('Hello Zell');
+// });
+gulp.task('sass', function() {
+  return gulp.src('app/assets/**/*.scss') // Gets all files ending with .scss in app/scss and children dirs
+    .pipe(sass())
+    .pipe(gulp.dest('app/css'))
+})
+
+
+gulp.task('watch', function(){
+  gulp.watch('app/assets/**/*.scss', ['sass']); 
+  // Other watchers
+})
